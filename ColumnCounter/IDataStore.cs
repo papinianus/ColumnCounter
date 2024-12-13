7da9ec8fa47ed6f6453ce3f3c0c1a6e1066085a1
kota2038 <99309760+kota2038@users.noreply.github.com>
2024-12-13 17:12:26 +0900

    Interface追加しました。

---
+++ ColumnCounter/IDataStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColumnCounter
+{
+    internal interface IDataStore
+    {
+        IEnumerable<string> LoadHeaders();
+        int CountColumn(string columnName);
+        IReadOnlyDictionary<string, int> CountColumns(IEnumerable<string> columnNames);
+    }
+}
